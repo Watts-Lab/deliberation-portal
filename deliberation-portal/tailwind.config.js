1493d12e65a61530f561e6679e9e59bce8b93e6f
kepstein23 <97930010+kepstein23@users.noreply.github.com>
2023-07-24 11:24:03 -0400

    basic config editor

--- deliberation-portal/tailwind.config.js
+++ deliberation-portal/tailwind.config.js
@@ -12,6 +12,20 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        empirica: {
+          50: "#fbfcfe",
+          100: "#f2f7fd",
+          200: "#bcd8f6",
+          300: "#8abbef",
+          400: "#549ce8",
+          500: "#237fe1",
+          600: "#1966b8",
+          700: "#124b87",
+          800: "#0c325a",
+          900: "#06192d",
+        },
+      },
     },
   },
   plugins: [],
